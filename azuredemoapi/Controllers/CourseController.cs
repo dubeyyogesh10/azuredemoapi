650eaafdcfd9e97d3c84ed7859333ab7754e9a32
dubeyyogesh10 <dubeyyogesh10@gmail.com>
2021-06-16 19:34:15 +0530

    Add project files.

---
+++ azuredemoapi/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using azuredemoapi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azuredemoapi.Controllers
+{
+    [ApiController]
+    [Route("/api/course")]
+    public class CourseController : ControllerBase
+    {
+
+        private CourseService _cs;
+
+        public CourseController(CourseService cs)
+        {
+            _cs = cs;
+        }
+
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok(_cs.GetCourses());
+        }
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ azuredemoapi/Services/CourseService.cs
@@ -0,0 +1,42 @@
+﻿using azuredemoapi.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azuredemoapi.Services
+{
+    public class CourseService
+    {
+        public List<Course> GetCourses()
+        {
+            return new List<Course>
+            {
+                new Course
+                {
+                    Id =1,
+                    Name = "english",
+                    Teacher = "Beta"
+                },
+                 new Course
+                {
+                    Id =2,
+                    Name = "maths",
+                    Teacher = "yo"
+                },
+                  new Course
+                {
+                    Id =3,
+                    Name = "english",
+                    Teacher = "Aplha"
+                },
+                   new Course
+                {
+                    Id =4,
+                    Name = "science",
+                    Teacher = "AB"
+                }
+            };
+        }
+    }
+}
